cc6c6189a0b14f72acb4e0ac3a6075f066f52d07
Farwa Rizvi <farwamehdi3@gmail.com>
2025-04-11 10:29:35 +0200

    styles added

--- src/app/singleview/[id]/page.jsx
+++ src/app/singleview/[id]/page.jsx
@@ -1,21 +1,6 @@
-// Next.js will invalidate the cache when a
-// request comes in, at most once every 60 seconds.
-
-// We'll prerender only the params from `generateStaticParams` at build time.
-// If a request comes in for a path that hasn't been generated,
-// Next.js will server-render the page on-demand.
-
 // import { getAnimals, getSingleAnimal } from "../../lib/api";
 import { getAnimals, getSingleAnimal } from "../../lib/api";
-
-// export async function generateStaticParams() {
-//   const data = await fetch("https://.../posts").then((res) => res.json());
-//   const data = await getAnimals();
-
-//   return data.map((animal) => ({
-//     id: animal.id,
-//   }));
-// }
+import { HiOutlineStar } from "react-icons/hi";
 
 export default async function Page({ params }) {
   const { id } = await params;
@@ -24,49 +9,58 @@ export default async function Page({ params }) {
   console.log("data: ", animalData);
 
   return (
-    <div>
-      <h1>{animalData.name}</h1>
-      <p>{animalData.description}</p>
-      <ul>{animalData.tags}</ul>
-      <img src={animalData.photos?.[0]?.medium} alt={animalData.name} />
+    <div className="mx-auto max-w-md rounded-3xl bg-white">
+      <div className="relative overflow-hidden rounded-3xl bg-[#F2968F] p-2">
+        <img
+          src={animalData.photos?.[0]?.medium}
+          alt={animalData.name}
+          className="mt-10 mb-6 h-100 w-full rounded-3xl object-cover"
+        />
+        <button className="absolute top-4 right-4 rounded-full bg-white/50 p-2 shadow">
+          <HiOutlineStar className="h-5 w-5 text-white" />
+        </button>
+      </div>
+      <div className="mx-auto max-w-md px-6 py-6">
+        <h1 className="mb-4 text-2xl font-bold">{animalData.name}</h1>
+        <p className="mb-4 text-sm leading-relaxed text-gray-700">
+          {animalData.description}
+        </p>
+        {/* <div className="">
+          <p className="line-height-6 mb-4 flex flex-row justify-evenly gap-2">
+            {animalData.tags}
+          </p>
+        </div> */}
+
+        <div className="mb-4 flex flex-wrap gap-2">
+          {animalData.tags?.map((tag, index) => (
+            <span
+              key={index}
+              className="rounded-full bg-pink-100 px-3 py-1 text-sm font-medium text-yellow-600 transition hover:bg-yellow-200"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+
+        <p className="text-xs text-gray-400">Opdateret den 13. juli.</p>
+        <div>
+          <button
+            style={{ backgroundColor: "#F2968F" }}
+            className="w-full cursor-pointer rounded-full px-6 py-3 font-semibold text-white"
+          >
+            Adopt Me
+          </button>
+        </div>
+      </div>
+      {/* <img src={animalData.photos?.[0]?.medium} alt={animalData.name} /> */}
     </div>
   );
 }
-
-// export async function Test({ params }) {
-//   const id = params.id;
-
-//   const animal = await getSingleAnimal(id, token);
-// }
-
-// export const dynamicParams = true; // or false, to 404 on unknown paths
-// export const revalidate = false;
-// // false | 0 | number
-
 // export async function generateStaticParams() {
-//   const posts = await fetch(`https://api.petfinder.com/v2/animals/${id}`).then(
-//     (res) => res.json(),
-//   );
-//   // Make sure to access `posts.data` since it's an object containing the list of animals
-//   return posts.data.map((post) => ({
-//     id: String(post.id),
+//   const data = await fetch("https://.../posts").then((res) => res.json());
+//   const data = await getAnimals();
+
+//   return data.map((animal) => ({
+//     id: animal.id,
 //   }));
 // }
-// // console.log(posts);
-// // export default async function Page({ params }) {
-// //   const { id } = params;
-// //   const post = await fetch(`https://api.petfinder.com/v2/animals/${id}`).then(
-// //     (res) => res.json(),
-// //   );
-// return (
-//   <main>
-//     <img
-//       src={post.photos[0]?.medium} // Make sure you're using `post` here instead of `data`
-//       alt={post.name} // Again, use `post` instead of `data`
-//       className="h-full w-full object-cover"
-//     />
-//     <h1>{post.name}</h1>
-//     <p>{post.description}</p>
-//     <p>{post.tags}</p>
-//   </main>
-// );
